9283df3963cc5e06deaacce44aec9a7d72127e90
Vicente Guedes <vicenteguedes91@gmail.com>
2025-05-06 09:18:06 +0200

    fix: add tvl and borrowed to spro

---
+++ projects/spro/index.js
@@ -0,0 +1,48 @@
+const headers = {
+  origin: "https://subgraph.satsuma-prod.com",
+  referer: "https://subgraph.satsuma-prod.com",
+};
+
+const subgraphUrl = "https://subgraph.satsuma-prod.com/28e9d214a5ae/stephanes-team/dev-spro-ethereum-subgraph/api";
+
+const getTokenMetrics = async () => {
+  const tokenMetricsQuery = `{
+    tokenMetrics_collection {
+      id
+      totalCollateralAmount
+      totalBorrowedAmount
+    }
+  }`;
+
+  const result = await fetch(subgraphUrl, {
+    method: "POST",
+    headers,
+    body: JSON.stringify({
+      query: tokenMetricsQuery,
+    }),
+  }).then((res) => res.json());
+  return result?.data?.tokenMetrics_collection || [];
+};
+
+async function getData(isBorrowed = false) {
+  const tokenMetrics = await getTokenMetrics();
+
+  return tokenMetrics.reduce((acc, token) => {
+    const totalBorrowedAmount = parseFloat(token.totalBorrowedAmount);
+
+    return {
+      ...acc,
+      [token.id]:
+        // We only need to add the total collateral amount if it's not borrowed
+        totalBorrowedAmount + parseFloat(isBorrowed ? 0 : token.totalCollateralAmount),
+    };
+  }, {});
+}
+
+module.exports = {
+  ethereum: {
+    tvl: () => getData(),
+    borrowed: () => getData(true),
+  },
+};
+// node test.js projects/spro/index.js
